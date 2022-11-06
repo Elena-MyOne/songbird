b21f00f4766d4661a9b222587022d3e1b18774f2
Elena <myone72@yahoo.com>
2022-11-05 22:13:56 -0400

    feat: add header

--- songbird/src/index.js
+++ songbird/src/index.js
@@ -1,21 +1,12 @@
-import './styles/styles.css';  //for css styles 
-import './styles/style.scss'
+import './styles/style.scss';
+import { headerBody } from './models/menu';
 
-import json from './assets/json.json'  //for json files import
-import xml from './assets/data.xml'  //for xml files import
+// import json from './assets/json.json'  //for json files import
 
-import Post from "@models/Post"; //js file example
-import WebpackLogo from './assets/webpack-logo' //js image example
-
-
-const post = new Post('Webpack Post Title', WebpackLogo)
-
-console.log('Post to String:', post.toString());
-
-console.log('JSON:', json)
-console.log('XML:', xml)
+// import Post from '@models/Post'; //js file example
 
 //!dynamic import example
-import('lodash').then(({default: lodash}) => {  //how to work with lodash 
-  console.log('lodash: ', lodash.random(0, 100, true))
-}) 
+// import('lodash').then(({ default: lodash }) => {
+//   //how to work with lodash
+//   console.log('lodash: ', lodash.random(0, 100, true));
+// });
---
+++ songbird/src/models/menu.js
@@ -0,0 +1,26 @@
+const headerBurger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+const headerLogo = document.querySelector('.header__logo');
+const headerLinks = document.querySelectorAll('.header__link');
+const headerList = document.querySelector('.header__list');
+
+headerBurger.addEventListener('click', toggleActive);
+headerLogo.addEventListener('click', toggleActive);
+
+function toggleActive() {
+  headerBurger.classList.toggle('active');
+  headerMenu.classList.toggle('active');
+  document.body.classList.toggle('lock');
+  headerLinks[0].classList.remove('_active-link');
+}
+
+headerList.addEventListener('click', function (event) {
+  if (event.target.closest('.header__link')) {
+    headerBurger.classList.remove('active');
+    headerMenu.classList.remove('active');
+    document.body.classList.remove('lock');
+    headerLinks[0].classList.add('_active-link');
+  }
+});
+
+export { headerBurger };
